a45649e8f9ba836e9898a72e2014657d49df5ab2
KeurePassos <129692173+KeurePassos@users.noreply.github.com>
2023-06-06 20:23:15 -0300

    Add files via upload

--- resources/script.sql
+++ resources/script.sql
@@ -5,4 +5,8 @@ create table contatos(
 id int not null auto_increment primary key,
 nome varchar(40),
 idade int,
-dataCadastro date)
+dataCadastro date
+email varchar(40) 
+telefone varchar(20) 
+endereco varchar(100)
+)
